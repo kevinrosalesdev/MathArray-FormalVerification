efcecc2397c74bb1a4459439190a737866a4d091
Kevindx98 <kevin6bepc@gmail.com>
2018-10-08 19:43:20 +0100

    Versión Prueba 2

--- src/main.adb
+++ src/main.adb
@@ -9,7 +9,7 @@ use Ada.Float_Text_IO;
 procedure Main is
    procedure Test_logarithm is
       Msg   : constant String := "Test_logarithm";
-      res :Float:=0.0;
+      res : Float := 0.0;
    begin
       logarithm(10, 100.0 , res);
       Assert_True (res = 2.0, Msg & " with a integer result in base 10");
@@ -46,8 +46,35 @@ procedure Main is
       when others =>
          Put_Line (Msg & " Failed (exception)");
    end Test_perpendicular_vec;
+
+   procedure Test_derivative is
+      Msg   : constant String := "Test_derivative";
+      v0 : vec(1) := (4);
+      v1 : vec(1..2) := (3,5);
+      v2 : vec(1..3) := (2,0,6);
+      v3 : vec(1..4) := (2,-6,0,10);
+      v4 : vec(1..5) := (0,3,6,-4,2);
+   begin
+      vecRes := derivative(v0);
+      Assert_True (v0(0) = 0, Msg & " with a function with only one coefficient.");
+      --vecRes := derivative(v1);
+      --Assert_True (res = 0.0, Msg & " with a zero result");
+      --vecRes := derivative(v2);
+      --Assert_True (res = 10.0, Msg & " with a integer result in base distinct to 10");
+      --vecRes := derivative(v3);
+      --Assert_True (res = 3.0, Msg & " with a integer result in base distinct to 10");
+      --vecRes := derivative(v4);
+      --Assert_True (res = 3.0, Msg & " with a integer result in base distinct to 10");
+   exception
+      when Assertion_Error =>
+         Put_Line (Msg & " Failed (assertion)");
+      when others =>
+         Put_Line (Msg & " Failed (exception)");
+   end Test_logarithm;
+
 begin
    Put_Line ("********************* Test_Max");
    Test_logarithm;
    Test_perpendicular_vec;
+   Test_derivative;
 end Main;
